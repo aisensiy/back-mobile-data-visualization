4609ee0748a28f8c90921dc6218ffd799c331a59
aisensiy <aisensiy@gmail.com>
2015-03-09 10:15:47 +0800

    add config file

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-from flask import Flask, jsonify, make_response
+from flask import Flask, make_response
 from json import dumps
 from flask.ext.cors import CORS
 import MySQLdb
+from config import HOST, USER, PASSWD, DATABASE
 
 app = Flask(__name__)
 cors = CORS(app)
 
-db = MySQLdb.connect('162.105.19.244', 'beijingmobile', 'KVision.im', 'mobile_data')
+db = MySQLdb.connect(HOST, USER, PASSWD, DATABASE)
 
 
 @app.route("/")
---
+++ config-example.py
@@ -0,0 +1,8 @@
+"""
+move this to config.py
+"""
+
+HOST = 'localhost'
+USER = 'root'
+PASSWD = '000000'
+DATABASE = 'tmp'
